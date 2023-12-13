bd69f7788a2576f05003ea347eb5e2f685e94f7c
Liam Keegan <liam@keegan.ch>
2023-12-13 08:26:35 +0100

    Notarize MacOS apps
    - add runtime hardening to signing (required for notarization)
    - notarize GUI app and CLI binary
      - macOS will then look-up this info when user tries to run the app
    - staple GUI app (binaries cannot be stapled)
      - stapling provides the same info offline
    - also notarize & staple the dmg for GUI and CLI

--- build.sh
+++ build.sh
@@ -64,16 +64,39 @@ security list-keychain -d user -s build.keychain
 security find-identity -v -p codesigning
 
 # sign app and binary
-/usr/bin/codesign --force -s "$MACOS_CERTIFICATE_NAME" app/spatial-model-editor.app -v
-/usr/bin/codesign --force -s "$MACOS_CERTIFICATE_NAME" cli/spatial-cli -v
+/usr/bin/codesign --force -s "$MACOS_CERTIFICATE_NAME" --options runtime app/spatial-model-editor.app -v
+/usr/bin/codesign --force -s "$MACOS_CERTIFICATE_NAME" --options runtime cli/spatial-cli -v
 /usr/bin/codesign -v app/spatial-model-editor.app
 
-# make dmg of binaries
-hdiutil create spatial-model-editor -fs HFS+ -srcfolder app/spatial-model-editor.app
+# notarize app and binary
+# store credentials to avoid UI prompts for passwords
+xcrun notarytool store-credentials "notarytool-profile" --apple-id "$MACOS_NOTARIZATION_APPLE_ID" --team-id "$MACOS_NOTARIZATION_TEAM_ID" --password "$MACOS_NOTARIZATION_PWD"
 
+# zip GUI app
+ditto -c -k --keepParent "app/spatial-model-editor.app" "notarization_gui.zip"
+# submit to be notarized
+xcrun notarytool submit "notarization_gui.zip" --keychain-profile "notarytool-profile" --wait
+# attach resulting staple to app for offline validation
+xcrun stapler staple "app/spatial-model-editor.app"
+# repeat for CLI - but note that although we can notarize a command line binary we can't staple it
+ditto -c -k --keepParent "cli/spatial-cli" "notarization_cli.zip"
+xcrun notarytool submit "notarization_cli.zip" --keychain-profile "notarytool-profile" --wait
+
+# make dmg of GUI app
+hdiutil create spatial-model-editor -fs HFS+ -srcfolder app/spatial-model-editor.app
+# notarize & staple the dmg
+ditto -c -k --keepParent "spatial-model-editor.dmg" "notarization_gui_dmg.zip"
+xcrun notarytool submit "notarization_gui_dmg.zip" --keychain-profile "notarytool-profile" --wait
+xcrun stapler staple "spatial-model-editor.dmg"
+
+# make dmg of CLI
 mkdir spatial-cli
 cp cli/spatial-cli spatial-cli/.
 hdiutil create spatial-cli -fs HFS+ -srcfolder spatial-cli
+# notarize & staple the dmg
+ditto -c -k --keepParent "spatial-cli.dmg" "notarization_cli_dmg.zip"
+xcrun notarytool submit "notarization_cli_dmg.zip" --keychain-profile "notarytool-profile" --wait
+xcrun stapler staple "spatial-cli.dmg"
 
 # display version
 ./app/spatial-model-editor.app/Contents/MacOS/spatial-model-editor -v
